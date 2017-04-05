da0ae7ff828e8a504395928a44c8db5a910a4f9d
Phillips <Tyrone.Phillips@sdsmt.edu>
2017-04-05 15:35:06 -0600

    fixed a small mistake

--- Matlab/functions/orUncertainty.m
+++ Matlab/functions/orUncertainty.m
@@ -3,29 +3,30 @@ function [U,pmean] = orUncertainty(f1,f2,f3,r12,r23,pformal)
 for i=1:numel(f1)
     p(i) = orderOfAccuracyOriginal(f1(i), f2(i), f3(i), r12, r23);
 end
-pmean=mean(p); %mean of order of accuracy, includes oscillatory and divergent cases as well
 
 for i = 1:numel(p)
     if p==-1
-        fprintf('Entry %4.0f is divergent\n',i);
+%         fprintf('Entry %4.0f is divergent\n',i);
         FS=3;
-        pU=0.5;
+        p(i)=0.5;
         
     elseif p==0
-        fprintf('Entry %4.0f is oscillatory\n',i);
+%         fprintf('Entry %4.0f is oscillatory\n',i);
         FS=3;
-        pU=0.5;
+        p(i)=0.5;
         
-    elseif abs(p-pformal)/pformal > 0.1
+    elseif abs(p(i)-pformal)/pformal > 0.1
         FS=3;
-        pU=min( max(0.5, p), pf );
+        p(i)=min( max(0.5, p(i)), pformal );
         
     else
         FS=1.25;
-        pU=pformal;
+        p(i)=pformal;
         
     end
 
-    U = FS*abs(f2-f1)./(r12^pU - 1);
+    U = FS*abs(f2-f1)./(r12^p(i) - 1);
 
 end
+
+pmean=mean(p); %mean of order of accuracy, includes oscillatory and divergent cases as well
